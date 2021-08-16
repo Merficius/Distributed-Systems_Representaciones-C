d53f5d4a8e08cc60a9b8adf7a92d5eff1fba0281
Mariano Portilla <chemanoportilla@hotmail.com>
2021-08-16 14:25:25 -0500

    Initial commit

---
+++ Representaciones-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Representaciones-C
+//
+//  Created by José Mariano Portilla Landa on 16/08/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
